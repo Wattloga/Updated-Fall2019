24fe47075c55f7af959de43466bd79c93dd1cf30
Logan W <wattloga@isu.edu>
2019-09-05 19:51:20 -0600

    Float Data
    Created a new script for float data, and added it to a script file under assets.

---
+++ **Scripting Unity**/Assets/Scripts/FloatData.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatData : ScriptableObject
+{
+    public float value = 1f;
+    public float maxValue = 1f; 
+
+    public void UpdateValue(float amount)
+    {
+        value += amount;
+        
+    }
+
+    public void UpdateValueLimitZero(float amount)
+    {
+        if (value < 0)
+        {
+            UpdateValue(amount);
+        }
+        else
+        {
+            value = maxValue;
+        }
+    }
+
+    public void UpdateValueLimitZeroAndMaxValue(float amount)
+    {
+       UpdateValueLimitZero(amount);
+        if (value <=maxValue)
+        {
+            UpdateValue(amount);
+        }
+        else
+        {
+            value = maxValue;
+        }
+        
+    }
+
+
+}
