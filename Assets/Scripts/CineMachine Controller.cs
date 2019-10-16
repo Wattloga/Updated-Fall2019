2d107ac6c79146cf7c289f928f056938fe3e705b
Logan W <wattloga@isu.edu>
2019-10-16 14:39:21 -0600

    Cinemachine Controller
    Made a Cinemachine controller for the camera in class.

---
+++ Assets/Scripts/CineMachine Controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class CineMachineController : MonoBehaviour
+{
+    public GameAction gameActionObj;
+    private CinemachineVirtualCamera virtualCamera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameActionObj.transformAction = TransformHandler;
+        virtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    private void TransformHandler(Transform transformObj)
+    {
+        virtualCamera.Follow = transformObj;
+    }
+   
+}
