7de76d964af60a7f7368ac9747272f2f2ce1c42e
Logan W <wattloga@isu.edu>
2019-10-16 14:37:55 -0600

    Instancer Script
    Created an instancer code in class

---
+++ Assets/Scripts/Instancer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Instancer : ScriptableObject
+{
+    public GameAction gameActionObj;
+    
+    public Vector3 instanceAtLocation;
+  
+    public void InstanceObj(GameObject obj)
+    {
+        var newObj = Instantiate(obj, instanceAtLocation, Quaternion.identity);
+        gameActionObj.transformAction(newObj.transform);
+    }
+    
+}
